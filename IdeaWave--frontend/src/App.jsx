a6dd55388fb3131a85235af74f21bc957d917eab
Oloishorua <jacoboloishorua@gmail.com>
2024-06-28 01:09:06 +0300

    implemented routing

--- IdeaWave--frontend/src/App.jsx
+++ IdeaWave--frontend/src/App.jsx
@@ -1,10 +1,22 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Register from './Pages/Register/Register';
+import Login from './Pages/Login/Login';
+import Home from "./Pages/Home"
+
 
 function App() {
 
   return (
     <>
-      <p>IdeaWave</p>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+        </Routes>
+      </BrowserRouter>
+
     </>
   )
 }
