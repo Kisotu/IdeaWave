1812df5f31bc44965af7354df6276d79459af07f
Oloishorua <jacoboloishorua@gmail.com>
2024-07-05 02:26:58 +0300

    home page added

--- IdeaWave--frontend/src/Pages/Home.jsx
+++ IdeaWave--frontend/src/Pages/Home.jsx
@@ -1,9 +1,22 @@
+import "../assets/css/style.css"
 
+
+import Navbar from "../components/Navbar"
+import Landing from "../components/Landing"
+import Hero from "../components/Hero"
+import Services from "../components/Services"
+import Pricing from "../components/Pricing"
+import Contact from "../components/Contact"
 
 export const Home = () => {
   return (
     <>
-      Home
+      <Navbar />
+      <Landing />
+      <Services />
+      <Hero />
+      <Pricing />
+      <Contact />
     </>
   )
 }
