b3123b4570518b4574762c8ad31a826fef162dce
Oloishorua <jacoboloishorua@gmail.com>
2024-07-05 02:25:37 +0300

    landing section component added

---
+++ IdeaWave--frontend/src/components/Landing.jsx
@@ -0,0 +1,44 @@
+
+const Landing = () => {
+	return (
+		<div className="main-banner" id="top">
+			<div className="container">
+				<div className="row">
+					<div className="col-lg-12">
+						<div className="row">
+							<div className="col-lg-6 align-self-center">
+								<div className="owl-banner">
+									<div className="item header-text">
+										<h6>Welcome to IdeaWave</h6>
+										<h2>
+											Where <em>trends</em> become business <span>Ideas</span>!
+										</h2>
+										<p>
+											We turn trending data into profitable business ideas aided by
+											state of the art analytic tools. Our data-driven approach helps you 
+											identify and capitalize on the next big thing. Turning insights into successful productss.
+										</p>
+										<div className="down-buttons">
+											<div className="main-blue-button-hover">
+												<a href="#contact">Message Us Now</a>
+											</div>
+											<div className="call-button">
+												<a href="#">
+													<i className="fa fa-phone" /> 0720675185
+												</a>
+											</div>
+										</div>
+									</div>
+
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+	)
+}
+
+export default Landing
