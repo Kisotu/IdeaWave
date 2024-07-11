6741e046687bc4b23804e50dc25b22fa66ed4a67
Oloishorua <jacoboloishorua@gmail.com>
2024-07-11 22:59:36 +0300

    added toggle function and destructured to calling components

--- IdeaWave--frontend/src/dashboard/Dashboard.jsx
+++ IdeaWave--frontend/src/dashboard/Dashboard.jsx
@@ -1,11 +1,21 @@
-
+import { useState } from "react"
+import "../dashboard/dashboard.css"
 import Header from "./components/Header"
 import Main from "./components/Main"
+import Sidebar from "./components/Sidebar"
+
 
 const Dashboard = () => {
+  const[openSidebarToggle, setOpenSibarToggle] = useState(false)
+
+  const OpenSidebar = () => {
+    setOpenSibarToggle(!openSidebarToggle)
+  }
+
   return (
-    <div className="container-scroller">
-      <Header />
+    <div className="grid-container">
+      <Header OpenSidebar={OpenSidebar}/>
+      <Sidebar openSidebarToggle={openSidebarToggle} OpenSidebar={OpenSidebar}/>
       <Main />
     </div>
   )
