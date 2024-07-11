a2d3b5e372c1691d9aa0d358966d90e75ece95e9
Oloishorua <jacoboloishorua@gmail.com>
2024-07-11 22:59:54 +0300

    main section of dashboard

---
+++ IdeaWave--frontend/src/dashboard/components/Main.jsx
@@ -0,0 +1,36 @@
+
+
+export const Main = () => {
+	return (
+		<main className="main-container">
+			<div className="main-title">
+				<h3>Dashboard</h3>
+			</div>
+			<div className="main-cards">
+				<div className="card">
+					<div className="card-inner">
+						<h3>Products 1</h3>
+					</div>
+				</div>
+				<div className="card">
+					<div className="card-inner">
+						<h3>Products 2</h3>
+					</div>
+				</div>
+				<div className="card">
+					<div className="card-inner">
+						<h3>Products 3</h3>
+					</div>
+				</div>
+				<div className="card">
+					<div className="card-inner">
+						<h4>Product 4</h4>
+					</div>
+				</div>
+
+			</div>
+		</main>
+	)
+}
+
+export default Main;
