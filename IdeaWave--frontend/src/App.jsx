a99e6447f0ad37e7f027e920f6ecb0b9f38cab2a
Oloishorua <jacoboloishorua@gmail.com>
2024-07-17 23:17:54 +0300

    updated routes and added nested routes for dashboard sidebar

--- IdeaWave--frontend/src/App.jsx
+++ IdeaWave--frontend/src/App.jsx
@@ -1,9 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import Register from './Pages/Register';
 import Login from './Pages/Login';
 import Home from "./Pages/Home";
-import Dashboard from  "./dashboard/Dashboard"
+import Dashboard from "./dashboard/Dashboard"
+// import Insights from "./dashboard/Insights"
+import Generate from "./dashboard/Generate"
+import Content from './dashboard/components/Content';
 
 
 function App() {
@@ -13,12 +16,17 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Home />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/register' element={<Register />} />
-          <Route path='/dashboard' element={<Dashboard />} />
+          <Route path='login' element={<Login />} />
+          <Route path='register' element={<Register />} />
+          <Route path='dashboard' element={<Dashboard />} >
+          <Route path='' element={<Navigate to="home" replace />} />
+            <Route path='home' element={<Content />} />
+            <Route path='home' element={<Dashboard />} />
+            {/* <Route path='insights' element={<Insights />}/> */}
+            <Route path='generate'element={<Generate />} />
+          </Route>
         </Routes>
       </BrowserRouter>
-
     </>
   )
 }
