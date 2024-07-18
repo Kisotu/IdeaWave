d70c0e4967bc6df0fc39707367ffa4890226e50c
Oloishorua <jacoboloishorua@gmail.com>
2024-07-18 11:25:39 +0300

    renders the generate ideas section

--- IdeaWave--frontend/src/dashboard/Generate.jsx
+++ IdeaWave--frontend/src/dashboard/Generate.jsx
@@ -1,10 +1,24 @@
-
+/* eslint-disable react/prop-types */
+import Trends from "../api/Trends"
+import OpenAi from "../api/OpenAI"
 
 const Generate = () => {
+
+
   return (
-	<div className="generate">
-    
-  </div>
+    <>
+      <div className="generate">
+        <h4 className="gen_title">Generate Insights</h4>
+      </div>
+
+      {/* Google Trends */}
+      <div className="trend-list">
+        <h4>Recent Trends</h4>
+        <Trends />
+      </div>
+
+      <OpenAi />
+    </>
   )
 }
 
